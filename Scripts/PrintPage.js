bc14abb06a6cfcebc1f06ee75dc5ee14bc745971
mike14420 <29795@HIR-MXL25120XH.whsnv.net>
2013-07-11 09:23:02 -0700

    moved the java script on the page to its own java file

---
+++ Scripts/PrintPage.js
@@ -0,0 +1,15 @@
+﻿
+
+
+$(document).ready(function () {
+
+    $("[id$=btnHideSearch]").click(function () {
+        $('#showButton').hide();
+        window.print();
+        window.open('/UsersView', 'UserView', null);
+    });
+
+
+
+
+});
